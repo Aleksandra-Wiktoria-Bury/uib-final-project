5a6ce0b56fbdebc5e21e0ed5c33bfe1a03990226
Aleksandra Bury <aleksandra.w.bury@gmail.com>
2021-05-23 15:17:33 +0200

    lightbox on Projects / restructured scss

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,1 +1,21 @@
-// Any code you will write for your website (in the future!) should go here
+const lightbox = document.createElement('div')
+lightbox.id = 'lightbox'
+document.body.appendChild(lightbox)
+
+const images = document.querySelectorAll('.img-project')
+images.forEach(image => {
+  image.addEventListener('click', e => {
+    lightbox.classList.add('active')
+    const img = document.createElement('img')
+    img.src = image.src
+    while (lightbox.firstChild) {
+      lightbox.removeChild(lightbox.firstChild)
+    }
+    lightbox.appendChild(img)
+  })
+})
+
+lightbox.addEventListener('click', e => {
+  if (e.target !== e.currentTarget) return
+  lightbox.classList.remove('active')
+})
